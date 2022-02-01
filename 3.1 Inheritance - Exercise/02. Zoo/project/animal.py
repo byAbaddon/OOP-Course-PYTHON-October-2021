8c9de79fb7b42fa7591d98309c5f78943ce20957
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2022-02-02 00:47:23 +0200

    Add files via upload

---
+++ 3.1 Inheritance - Exercise/02. Zoo/project/animal.py
@@ -0,0 +1,11 @@
+class Animal:
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+
+
+
---
+++ 3.1 Inheritance - Exercise/02. Zoo/project/bear.py
@@ -0,0 +1,8 @@
+from project.mammal import Mammal
+
+
+class Bear(Mammal):
+    def __init__(self, name):
+        Mammal.__init__(self, name)
+
+
